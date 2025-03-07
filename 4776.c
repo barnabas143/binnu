5086093777774c9babcb9752c06d524cf3c07c03
barnabas143 <barnabas6287@gmail.com>
2025-03-07 11:29:07 +0530

    Add files via upload

---
+++ 4776.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int countways(int coins[],int n,int sum){
+	int dp[sum+i];
+	for(int i=0;i<=sum;i++)
+	
+	dp[i]=0;
+	dp[0]=1;
+
+	for(int i=0;i<n;i++){
+		for(int j=coins[i];j<=sum;j++){
+			dp[j]+=dp[j-coins[i]];
+		}
+	}
+	return dp[sum];
+}
+int main(){
+	int n,sum;
+	printf("enter the number of coins: ");
+	scanf("%d",&n);
+	int coins[n];
+	printf("enter the denominations of code: ");
+	for(int i=0;i<n;i++){
+		scanf("%d",&coins[i]);
+	}
+	printf("enter the sum values: ");
+	scanf("%d",&sum);
+	int result=countways(coins,,sum);
+	printf("number of ways to make sum %d: "sum,result);
+	return 0;
+}
